c8bd55004f535f7746b0d18382b11ada8edb5fea
sankar <95sankar@gmail.com>
2024-04-26 07:32:42 +0530

    bug fixs

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   api: {
     //API_URL: "https://api-node.jeesofttech.website",
-    API_URL: "http://localhost:5000/api/v1",
+    API_URL: `${REACT_APP_API_URL}api/v1`,
   },
 };
